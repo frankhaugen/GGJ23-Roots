549293cdba41cf69d033f8e0c06309e17ded359a
Frank R. Haugen <frank.haugen@gmail.com>
2023-02-04 18:16:29 +0100

    Added startup initialization and main menu scene

---
+++ Assets/Scripts/Initialization/InitializeGame.cs
@@ -0,0 +1,38 @@
+﻿
+using UnityEngine;
+
+public class InitializeGame
+{
+    [RuntimeInitializeOnLoadMethod]
+    public static void InitializeFiles()
+    {
+        InitializeDirectories();
+        InitializeGameSettings();
+        
+        Debug.Log("Game Initialized");
+    }
+    
+    public static void InitializeDirectories()
+    {
+        if (!Directories.GameFiles.Exists) Directories.GameFiles.Create();
+        if (!Directories.Saves.Exists) Directories.Saves.Create();
+        if (!Directories.Autosaves.Exists) Directories.Autosaves.Create();
+        if (!Directories.Screenshots.Exists) Directories.Screenshots.Create();
+        if (!Directories.Logs.Exists) Directories.Logs.Create();
+        if (!Directories.PlayerSettings.Exists) Directories.PlayerSettings.Create();
+    }
+
+    public static void InitializeGameSettings()
+    {
+        if (!Files.GameSettings.Exists)
+        {
+            var defaultGameSettings = new GameSettings()
+            {
+                Difficulty = Difficulty.Normal,
+                GameName = "Roots of Destiny"
+            };
+            var gameSettings = SerializationUtility.Serialize(defaultGameSettings);
+            FileManager.Write(Files.GameSettings, gameSettings);
+        }
+    }
+}
