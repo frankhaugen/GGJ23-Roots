67928ddb8b98f6f59f51c9b4d681b67d8b44fff0
Frank R. Haugen <frank.haugen@gmail.com>
2023-02-04 21:42:59 +0100

    Added state tracking

---
+++ Assets/Scripts/EqualityComparers/InvariantCaselessStringComparer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class InvariantCaselessStringComparer : IEqualityComparer<string>
+{
+    public bool Equals(string x, string y)
+    {
+        return string.Equals(x, y, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    public int GetHashCode(string obj)
+    {
+        return obj.ToLowerInvariant().GetHashCode();
+    }
+}
---
+++ Assets/Scripts/GameState/GameStateTracker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public static class GameStateTracker
+    {
+        private static readonly Dictionary<string, PlayerInfo> _players = new Dictionary<string, PlayerInfo>(new InvariantCaselessStringComparer());
+        
+        public static void SaveGameObjectState(GameObject gameObject)
+        {
+            var playerInfo = gameObject.ToPlayerInfo();
+            
+            if (_players.ContainsKey(playerInfo.Name))
+            {
+                _players[playerInfo.Name] = playerInfo;
+            }
+            else
+            {
+                _players.Add(playerInfo.Name, playerInfo);
+            }
+        }
+    }
